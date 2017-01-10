f43888ddb481bac974d201840664e15ed007c5a3
yangyao <yangyaohappy@outlook.com>
2017-01-10 19:04:15 +0800

    bug fixed for unicode error

--- manage.py
+++ manage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+# encoding: utf-8
 import os
 import sys
+
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+# 不知道上面是什么神奇代码总之管用了
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yangyao.settings")
